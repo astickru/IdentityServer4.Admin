b496f51f6f239df1f32f48b5dfcc8833cfcac1a9
a-zaycev <zaycev_alexandr@bk.ru>
2020-03-27 08:32:30 +0300

    Добавил idext в токен

---
+++ src/Iserv.IdentityServer4.BusinessLogic/Services/IdentityClaimsProfileService.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Iserv.IdentityServer4.BusinessLogic.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Shared.Entities.Identity;
+
+namespace Iserv.IdentityServer4.BusinessLogic.Services
+{
+    public class IdentityClaimsProfileService : IProfileService
+    {
+        private readonly IUserClaimsPrincipalFactory<UserIdentity> _claimsFactory;
+        private readonly UserManager<UserIdentity> _userManager;
+
+        public IdentityClaimsProfileService(UserManager<UserIdentity> userManager, IUserClaimsPrincipalFactory<UserIdentity> claimsFactory)
+        {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var claims = principal.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+            claims.Add(new Claim(UserClaimsHelpers.FieldIdExt, user.Idext.ToString()));
+            claims.Add(new Claim(IdentityServerConstants.StandardScopes.Email, user.Email));
+            claims.AddRange(roles.Select(role => new Claim(JwtClaimTypes.Role, role)));
+            context.IssuedClaims = claims;
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+}
