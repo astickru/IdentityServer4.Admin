78eda1767e21feff06c22f3a112c8ca2b79f8ff3
a-zaycev <zaycev_alexandr@bk.ru>
2020-04-06 08:28:37 +0300

    Добавил миграцию БД аналогичную боевой. Промежуточная фиксация интеграции TouchId и FaceId

---
+++ src/Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL/Migrations/Identity/20200401101307_dbIdtDeviceId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skoruba.IdentityServer4.Admin.EntityFramework.PostgreSQL.Migrations.Identity
+{
+    public partial class dbIdtDeviceId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserDevicesId",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DeviceId = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    DeviceIdType = table.Column<byte>(nullable: false),
+                    DeviceName = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDevicesId", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserDevicesId_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDevicesId_UserId",
+                table: "UserDevicesId",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDevicesId");
+        }
+    }
+}
